c62b2f19208f1c325a1602628fe139b6c1a8afe2
rofehr <roland@diefehrs.de>
2026-02-04 13:17:46 +0100

    asdasdas

--- menu_plugins.h
+++ menu_plugins.h
@@ -2,6 +2,15 @@
 #define __MENU_PLUGINS_H
 
 #include <vdr/osdbase.h>
+#include <vdr/plugin.h>
+
+class cPluginMenuItem : public cOsdItem {
+public:
+	cPlugin *plugin;
+	cPluginMenuItem(cPlugin *Plugin)
+	: cOsdItem(Plugin->MainMenuEntry()), plugin(Plugin) {}
+};
+
 
 class cOsdMenuSortedPlugins : public cOsdMenu {
 public:
