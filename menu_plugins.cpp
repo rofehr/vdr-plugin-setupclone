016338a7639712f0b375f9b3bae988daffad6a7b
rofehr <roland@diefehrs.de>
2026-02-04 11:19:16 +0100

    dasdas

--- menu_plugins.cpp
+++ menu_plugins.cpp
@@ -1,21 +1,45 @@
 #include "menu_plugins.h"
 #include "setupclone.h"
+
 #include <vdr/plugin.h>
-#include <algorithm>
+#include <vdr/menuitems.h>
+
+class cPluginMenuItem : public cOsdItem {
+private:
+  cPlugin *plugin;
+public:
+  cPluginMenuItem(cPlugin *Plugin)
+  : cOsdItem(Plugin->MainMenuEntry()), plugin(Plugin) {}
+
+  eOSState ProcessKey(eKeys Key) override {
+    if (Key == kOk)
+      return plugin->MainMenuAction();
+    return osContinue;
+  }
+};
 
 cOsdMenuSortedPlugins::cOsdMenuSortedPlugins()
 : cOsdMenu(tr("Plugins"))
 {
+  // 1. Sortierte Plugins
   for (const auto &name : PluginOrder) {
     cPlugin *p = cPluginManager::GetPlugin(name.c_str());
     if (p && p->MainMenuEntry())
-      Add(new cMenuPluginItem(p));
+      Add(new cPluginMenuItem(p));
   }
 
+  // 2. Restliche Plugins anhängen
   for (cPlugin *p = cPluginManager::First(); p; p = cPluginManager::Next(p)) {
-    if (!p->MainMenuEntry()) continue;
-    if (std::find(PluginOrder.begin(), PluginOrder.end(), p->Name()) == PluginOrder.end())
-      Add(new cMenuPluginItem(p));
+    if (!p->MainMenuEntry())
+      continue;
+
+    bool known = false;
+    for (auto &n : PluginOrder)
+      if (n == p->Name())
+        known = true;
+
+    if (!known)
+      Add(new cPluginMenuItem(p));
   }
 
   Display();
